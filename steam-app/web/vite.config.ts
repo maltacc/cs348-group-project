73abb517a4d171db87f60f4cfb5ab694f95c0a5e
Raymond Mei <raymondmei.rm@gmail.com>
2025-11-12 01:06:39 -0500

    install shadcn styling system

--- steam-app/web/vite.config.ts
+++ steam-app/web/vite.config.ts
@@ -1,6 +1,13 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173, proxy: { "/api": "http://localhost:3001" } }
+	plugins: [react(), tailwindcss()],
+	server: { port: 5173, proxy: { "/api": "http://localhost:3001" } },
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
